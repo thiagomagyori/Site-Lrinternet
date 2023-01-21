7e47d3e5e6d29b95cdfb0d67d390adb447d0f4cf
Thiago Magyori <thiagosilvamagyori@mail.com>
2023-01-21 17:32:26 -0300

    feito galeria de fotos e footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 window.sr = ScrollReveal ({reset: true});
 
-sr.reveal('.aria-2', {duration: 3000});
+sr.reveal('.aria-2', {duration: 2000});
+
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.sobre-empresa', {duration: 2000});
+
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.galeria', {duration: 2000});
