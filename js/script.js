fca28e1bc1876523972d1c02cdad185488917718
Thiago Magyori <thiagosilvamagyori@mail.com>
2023-01-22 17:07:16 -0300

    footer pronto e responsividade para mobile

--- js/script.js
+++ js/script.js
@@ -5,7 +5,3 @@ sr.reveal('.aria-2', {duration: 2000});
 window.sr = ScrollReveal ({reset: true});
 
 sr.reveal('.sobre-empresa', {duration: 2000});
-
-window.sr = ScrollReveal ({reset: true});
-
-sr.reveal('.galeria', {duration: 2000});
